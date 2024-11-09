5d9070b5cab786add711031cf081b9deb542cee8
Agustin Kassis <webmaster@masize.com>
2024-11-08 21:07:21 -0300

    update deploy link

--- app/page.tsx
+++ app/page.tsx
@@ -34,7 +34,7 @@ export default function Home() {
         <div className='flex gap-4 items-center flex-col sm:flex-row'>
           <a
             className='rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5'
-            href='[![Deploy with Vercel](https://vercel.com/button)](https://vercel.com/new/clone?repository-url=https%3A%2F%2Fgithub.com%2Flacrypta%2Fsatsconf)'
+            href='https://vercel.com/new/clone?repository-url=https%3A%2F%2Fgithub.com%2Flacrypta%2Fsatsconf'
             target='_blank'
             rel='noopener noreferrer'
           >
